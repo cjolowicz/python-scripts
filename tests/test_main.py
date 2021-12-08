61b1cf35aeef59b31cec3625b5b06b3863ca46b8
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-12-08 13:33:13 +0100

    🔥 Remove placeholder CLI

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,9 @@
 """Test cases for the __main__ module."""
 import pytest
+from click import BaseCommand
 from click.testing import CliRunner
 
-from cjolowicz_scripts import __main__
+from cjolowicz_scripts import dependabot_rebase_all
 
 
 @pytest.fixture
@@ -11,7 +12,11 @@ def runner() -> CliRunner:
     return CliRunner()
 
 
-def test_main_succeeds(runner: CliRunner) -> None:
+@pytest.mark.parametrize(
+    "main",
+    [dependabot_rebase_all.main],
+)
+def test_help(runner: CliRunner, main: BaseCommand) -> None:
     """It exits with a status code of zero."""
-    result = runner.invoke(__main__.main)
+    result = runner.invoke(main, ["--help"])
     assert result.exit_code == 0
